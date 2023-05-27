bdfcfeb8604d42ff01a67c6599dd06092091d4e0
martinzaj <mzajdenberg@frba.utn.edu.ar>
2023-05-27 15:13:22 -0300

    actualizacion

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-var cantEntradasParaComprar = 0;
-var entradasDisponibles = 8000;
+let cantEntradasParaComprar = 0;
+let entradasDisponibles;
+
+
+class Evento {
+    constructor(titulo, ubicacion, horario, entradasDisponibles, precio, artista, dia) {
+        this.titulo = titulo;
+        this.ubicacion = ubicacion;
+        this.horario = horario
+
+        this.entradasDisponibles = entradasDisponibles
+
+        this.precio = precio;
+        this.artista = artista;
+        this.dia = dia
+    }
+}
+const evento1 = new Evento("moon time", "Cordoba 1200", "21:30", 2000, 7500, "King Moon", "5/3/23");
+const evento2 = new Evento("Ultima gira", "Hipodrimo", "19:00", 15000, 24000, "Kiss", "23/9/23");
+const evento3 = new Evento("butakera", "Santa Fe", "23:50", 500, 1200, "La joaki");
+
 
 function verEvento() {
 
@@ -55,6 +74,24 @@ function hacerEntradasParaComprar(cantEntradasParaComprar, botonMenosPresionado,
 
 function hacerEntradasRestantes(entradasDisponibles, entradasParaComprar) { return entradasDisponibles - entradasParaComprar }
 
+
+let numEvento = prompt("¿que evento desea ver (debe ser numerico)")
+switch (numEvento) {
+
+    case 1:
+        entradasDisponibles= evento1.entradasDisponibles
+        break;
+    case 2:
+        entradasDisponibles= evento2.entradasDisponibles
+        break;
+    
+    case 3:
+        entradasDisponibles= evento3.entradasDisponibles
+        break;
+}
 verEvento()
 entradasDisponibles = hacerEntradasRestantes(entradasDisponibles, cantEntradasParaComprar);
+//esto genera muchos switch mientras mas eventos hay; me preocupa
 alert("las entradas disponibles son " + entradasDisponibles)
+evento1.entradasDisponibles =  entradasDisponibles;
+
