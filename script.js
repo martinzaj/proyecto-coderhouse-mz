95c099cb467016e6f1c9120cdb7912a0fb3479b1
martinzaj <mzajdenberg@frba.utn.edu.ar>
2023-06-03 19:56:36 -0300

    actualizacion sin el switch

--- script.js
+++ script.js
@@ -15,9 +15,14 @@ class Evento {
         this.dia = dia
     }
 }
-const evento1 = new Evento("moon time", "Cordoba 1200", "21:30", 2000, 7500, "King Moon", "5/3/23");
-const evento2 = new Evento("Ultima gira", "Hipodrimo", "19:00", 15000, 24000, "Kiss", "23/9/23");
-const evento3 = new Evento("butakera", "Santa Fe", "23:50", 500, 1200, "La joaki");
+const eventos = {
+    evento1: new Evento("Moon time", "Cordoba 1200", "21:30", 2000, 7500, "King Moon", "5/3/23"),
+    evento2: new Evento("Ultima gira", "Hipodrimo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
+    evento3: new Evento("Butakera", "Santa Fe", "23:50", 500, 1200, "La joaki"),
+    evento3: new Evento("Swift tour", "River", "20:50", 15000,  30000, "Taylow Swift"),
+    evento4: new Evento("XYZ", "Armenia", "17:00", 80, 500, " "),
+    evento5: new Evento("Aveces", "Cervantes", "23:00", 1000, 3000, "Zack")
+  };
 
 
 function verEvento() {
@@ -75,23 +80,24 @@ function hacerEntradasParaComprar(cantEntradasParaComprar, botonMenosPresionado,
 function hacerEntradasRestantes(entradasDisponibles, entradasParaComprar) { return entradasDisponibles - entradasParaComprar }
 
 
-let numEvento = prompt("¿que evento desea ver (debe ser numerico)")
-switch (numEvento) {
 
-    case 1:
-        entradasDisponibles= evento1.entradasDisponibles
-        break;
-    case 2:
-        entradasDisponibles= evento2.entradasDisponibles
-        break;
-    
-    case 3:
-        entradasDisponibles= evento3.entradasDisponibles
-        break;
-}
+
+let numEvento =prompt("¿Qué evento desea ver? (debe ser numérico)");
+
+let eventoSeleccionado = `evento${ numEvento}`;
+
+if (eventos.hasOwnProperty(eventoSeleccionado)) {
+    entradasDisponibles = eventos[eventoSeleccionado].entradasDisponibles;
+    alert("las entradas disponibles para "  + eventos[eventoSeleccionado].titulo + " son " + entradasDisponibles)
+  } else {
+    console.log("Evento no encontrado");
+  }
+
+
 verEvento()
 entradasDisponibles = hacerEntradasRestantes(entradasDisponibles, cantEntradasParaComprar);
-//esto genera muchos switch mientras mas eventos hay; me preocupa
-alert("las entradas disponibles son " + entradasDisponibles)
-evento1.entradasDisponibles =  entradasDisponibles;
+console.log(entradasDisponibles);
+
+alert("las entradas restantes luego de su compra para "  + eventos[eventoSeleccionado].titulo + " son " + entradasDisponibles)
+eventos[eventoSeleccionado].entradasDisponibles = entradasDisponibles;
 
