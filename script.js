523b07a9f3278bf0ab289bd97ccf9f045f2a0e7e
martinzaj <mzajdenberg@frba.utn.edu.ar>
2023-06-22 19:26:58 -0300

    reahecho

--- script.js
+++ script.js
@@ -1,69 +1,15 @@
-let cantEntradasParaComprar = 0;
-let entradasDisponibles;
-let cantEntradasElement = document.getElementById("cant-entradas");
-let eventoSeleccionado;
-
-// Función para agregar o restar entradas
-function hacerEntradasParaComprar(cantidad, restar, sumar) {
-    if (restar) {
-        cantidad--;
-    }
-    if (sumar) {
-        cantidad++;
-    }
-    return cantidad;
+document.addEventListener("DOMContentLoaded", cargaInicial);
+let Precioserivice = 77.7;
+function cargaInicial() {
+    cargarCarritoDeLocalStorage()
+    renderizarProductos();
+    renderizarCarrito();
 }
 
-function hacerEntradasRestantes(entradasDisponibles, entradasParaComprar) {
-    return entradasDisponibles - entradasParaComprar;
-}
-
-function seleccionarEvento(evento) {
-    eventoSeleccionado = evento;
-}
-
-function buscarPorArtista() {
-    let artistaBuscado = prompt("¿Qué artista desea buscar?");
-    const artistasEncontrados = eventos.filter((el) =>el.artista.includes(artistaBuscado)
-    );
-    console.table(artistasEncontrados);
-}
-
-function buscarTodosLosEventos() {
-    let respuesta = prompt("¿Quiere ver todos los eventos? (si/no)");
-    respuesta = respuesta.toLowerCase();
-
-    if (respuesta === "si") {
-        const todosLosEventos = (eventos).map((el) => {
-            return {
-                Artista: el.artista,
-                Evento: el.titulo,
-                Dia: el.dia,
-                Precio: el.precio,
-            };
-        });
-
-        console.table(todosLosEventos);
-    }
-}
-
-function hacerLaCompra() {
-    const hacerLaCompra = confirm("¿Quiere acceder a este evento?");
-    if (hacerLaCompra) {
-        alert("¡Haga su compra!");
-    }
-}
-
-
-
-function actualizarCantidadEntradas() {
-    cantEntradasElement.textContent = cantEntradasParaComprar;
-    entradasDisponibles = hacerEntradasRestantes(evento.entradasDisponibles, cantEntradasParaComprar);
-    console.log(entradasDisponibles);
-}
 
 class Evento {
-    constructor(titulo, ubicacion, horario, entradasDisponibles, precio, artista, dia) {
+    constructor(id, titulo, ubicacion, horario, entradasDisponibles, precio, artista, dia) {
+        this.id = id;
         this.titulo = titulo;
         this.ubicacion = ubicacion;
         this.horario = horario;
@@ -73,157 +19,221 @@ class Evento {
         this.dia = dia;
     }
 }
-
-eventos = [
-    new Evento("Moon time", "Cordoba 1200", "21:30", 2000, 7500, "King Moon", "5/3/23"),
-    new Evento("Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
-    new Evento("Butakera", "Santa Fe", "23:50", 500, 1200, "La joaki", ""),
-    new Evento("Swift tour", "River", "20:50", 15000, 30000, "Taylor Swift", "30/5/24"),
-    new Evento("XYZ", "Armenia", "17:00", 80, 500, "Artista 0", "Fecha 0"),
-    new Evento("Aveces", "Cervantes", "23:00", 1000, 3000, "Zack", "Fecha 0"),
-    new Evento("Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
-    new Evento("Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
-    new Evento("Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
-    new Evento("Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
-    new Evento("Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
-    new Evento("Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
-]
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    const contenedorEventos = document.getElementById("contenedor-de-eventos");
+const eventos = [
+    new Evento(1, "Moon time", "Cordoba 1200", "21:30", 2000, 7500, "King Moon", "5/3/23"),
+    new Evento(2, "Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
+    new Evento(3, "Butakera", "Santa Fe", "23:50", 500, 1200, "La joaki", "sin fijar"),
+    new Evento(4, "Swift tour", "River", "20:50", 15000, 30000, "Taylor Swift", "30/5/24"),
+    new Evento(5, "XYZ", "Armenia", "17:00", 80, 500, "Artista x", "Fecha 0"),
+    new Evento(6, "Aveces", "Cervantes", "23:00", 1000, 3000, "Zack", "Fecha 0"),
+    new Evento(7, "Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
+    new Evento(8, "Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
+    new Evento(9, "Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
+    new Evento(10, "Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
+    new Evento(11, "Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
+    new Evento(12, "Ultima gira", "Hipodromo", "19:00", 15000, 24000, "Kiss", "23/9/23"),
     
+]
+const d = document;
+let CARRITO = [];
 
-for (let i = 0; i < eventos.length; i++) {
-        const evento = eventos[i];
 
-        const column = document.createElement("div");
+ // Obtener referencias a los elementos del buscador
+ const searchInput = document.getElementById("searchInput");
+ const searchButton = document.getElementById("searchButton");
+ searchButton.addEventListener('click', () => {
+   const searchText = searchInput.value; // Obtener el texto de búsqueda sin espacios en blanco
+   buscarEventosPorArtista(searchText);
+ })
+function buscarEventosPorArtista(searchText){
+    const artistasEncontrados = eventos.filter((el) => el.artista.includes(searchText)
+    );
+    console.table(artistasEncontrados);
+}
+
+//boton para comprar todos los productos del carrito
+const ConfBuy = document.getElementById("buy");
+ConfBuy.addEventListener('click', () => { // Agregar evento de clic al botón
+    let x = 0; x = calcularTotal()
+    if (confirm("quiere realizar la compra de sus productos? La cantidad de dindero a pagar es: " + x)) {
+        comprarLosProdutos()
+    }
+});
+function calcularTotal() {
+    let PrecioAPagar = 0;
+    CARRITO.forEach((p) => {
+        PrecioAPagar += p.cantidad * p.precio;
+    });
+    PrecioAPagar += Precioserivice;
+    PrecioAPagar = PrecioAPagar == Precioserivice ? 0 : PrecioAPagar;
+    return PrecioAPagar;
+}
+function comprarLosProdutos() {
+    CARRITO = []; // Vaciar el carrito después de calcular el precio total
+    renderizarCarrito();
+    guardarCarritoEnLocalStorage();
+}
+function renderizarProductos() {
+
+    const $tienda = d.getElementById("tienda");
+    let fila;
+
+    eventos.forEach((p, index) => {
+
+        if (index % 3 === 0) {
+            // Cada tres productos, creamos una nueva fila
+            fila = d.createElement('div');
+            fila.classList.add('row');
+            $tienda.appendChild(fila);
+        }
+
+        //atajo
+        let producto = d.createElement('div')
+
+        //generacion de los cards
+        producto.classList.add('col-md-4');
+        const column = producto
         column.classList.add("col-md-4");
-
-        const card = document.createElement("div");
+        const card = d.createElement("div");
         card.classList.add("card");
         card.style.width = "18rem";
-
-        const cardBody = document.createElement("div");
+        const cardBody = d.createElement("div");
         cardBody.classList.add("card-body");
-
-        const title = document.createElement("h5");
+        const title = d.createElement("h5");
         title.classList.add("card-title");
-        title.textContent = evento.artista;
-
-        const description = document.createElement("p");
+        title.textContent = eventos.artista;
+        const description = d.createElement("p");
         description.classList.add("card-text");
-        description.textContent = `Evento: ${evento.titulo}
-        Día: ${evento.dia}
-        Precio: ${evento.precio}`;
+        description.innerHTML = `Artista: ${p.artista}  Evento: ${p.titulo}   Día: ${p.dia}    Precio: ${p.precio}`;
+        const link = d.createElement("a");
+        //se inserta el boton
+        const verEventoBtn = d.createElement("button");
+        verEventoBtn.classList.add("btn", "btn-primary", "mt-3");
+        verEventoBtn.textContent = "Ver información del evento";
 
-        const link = document.createElement("a");
-        link.href = "#info-evento";
-        link.classList.add("btn", "btn-primary");
-        link.textContent = "Comprar";
+        verEventoBtn.addEventListener('click', () => { // Agregar evento de clic al botón
+            generarEvento(p);
+            link.href = "#info-evento";
+        });
 
+        //se insertan todos los elementos 
         cardBody.appendChild(title);
         cardBody.appendChild(description);
+        cardBody.appendChild(verEventoBtn);
         cardBody.appendChild(link);
         card.appendChild(cardBody);
         column.appendChild(card);
-        contenedorEventos.appendChild(column);
 
-link.addEventListener("click", function () {
-            event.preventDefault();
-            seleccionarEvento(evento);
-            const infoEvento = document.getElementById("info-evento");
-            infoEvento.classList.add("mb-3");
-            infoEvento.classList.add("custom-modal-content");
-            infoEvento.innerHTML = `<div class="modal-dialog modal-dialog-centered"><div class="modal-content"><div class="modal-header text-center">
-      <h5 class="modal-title text-center">Información del evento</h5></div><div class="modal-body"><div class="bg-purple rounded p-3 text-center">
-                <p class="mb-0">Título: ${evento.titulo}</p>
-                <p class="mb-0">Ubicación: ${evento.ubicacion}</p>
-                <p class="mb-0">Horario: ${evento.horario}</p>
-                <p class="mb-0">Entradas disponibles: ${evento.entradasDisponibles}</p>
-                <p class="mb-0">Precio: ${evento.precio}</p>
-                <p class="mb-0">Artista: ${evento.artista}</p>
-                <p class="mb-0">Día: ${evento.dia}</p></div></div></div></div>`;
 
-            const comprarBtnContainer = document.getElementById("comprar-btn-container");
-            comprarBtnContainer.classList.add("mt-3");
-            comprarBtnContainer.innerHTML = `
-        <div class="text-center">
-          <button id="comprar-btn" class="btn btn-primary mt-3">Comprar entradas</button>
-        </div>
-      `;
-            let carritoGenerado = false;
+        producto.appendChild(card);
+        fila.appendChild(producto);
+    })
 
-            const comprarBtn = document.getElementById("comprar-btn");
-            comprarBtn.addEventListener("click", function () {
-                event.preventDefault();
-                if (!carritoGenerado) {
-                    const carrito = document.createElement("div");
-                    carrito.classList.add("carrito");
-                    carrito.innerHTML = `
-            <button class="btn btn-primary btn-sm carrito-btn">
-              <i class="fas fa-shopping-cart"></i>
-            </button>
-            <button class="btn btn-primary btn-sm carrito-btn-mas">+</button>
-            <button class="btn btn-primary btn-sm carrito-btn-menos">-</button>
-          `;
-                    comprarBtnContainer.appendChild(carrito);
-                    carritoGenerado = true;
-                }
+    function generarEvento(p) {
+        //generacion de la informacion del evento particular que se selecciono
+        const infoEvento = document.getElementById("info-evento");
 
-                ComprarCarrito();
-                hacerLaCompra();
-            });
+        infoEvento.classList.add("mb-3");
+        infoEvento.classList.add("custom-modal-content");
+        infoEvento.innerHTML = `<div calass="modal-dialog modal-dialog-centered"><div class="modal-content"><div class="modal-header text-center">
+  <h5 class="modal-title text-center">Información del evento</h5></div><div class="modal-body"><div class="bg-purple rounded p-3 text-center">
+            <p class="mb-0">Título: ${p.titulo}</p>
+            <p class="mb-0">Ubicación: ${p.ubicacion}</p>
+            <p class="mb-0">Horario: ${p.horario}</p>
+            <p class="mb-0">Entradas disponibles: ${p.entradasDisponibles}</p>
+            <p class="mb-0">Precio: ${p.precio}</p>
+            <p class="mb-0">Artista: ${p.artista}</p>
+            <p class="mb-0">Id: ${p.id}</p>
+            <p class="mb-0">Día: ${p.dia}</p></div></div></div></div>`;
+        //boton de agregar al carrito
+        const comprarBtnContainer = document.getElementById("comprar-btn-container");
+        comprarBtnContainer.classList.add("mt-3");
+        comprarBtnContainer.innerHTML = ` <div class="text-center"   id="comprar-btn">
+        <button id="${p.id}" class="btn btn-primary mt-3">Agregar al carrito</button> </div>`;
 
-           
-            function ComprarCarrito() {
-                
-                let hacerLaCompra = false;
-                let botonMenosPresionado = false;
-                let botonMasPresionado = false;
-
-                cantEntradasElement.innerHTML = `<p cant-entradas></p>`
-                cantEntradasElement.textContent = cantEntradasParaComprar;
-
-                const botonMenos = document.querySelector(".carrito-btn-menos");
-                botonMenos.addEventListener("click", function () {
-                    botonMenosPresionado = true;
-                    cantEntradasParaComprar = hacerEntradasParaComprar(cantEntradasParaComprar, botonMenosPresionado, botonMasPresionado);
-                    botonMenosPresionado = false;
-
-                    actualizarCantidadEntradas();
-                });
-
-                const botonMas = document.querySelector(".carrito-btn-mas");
-                botonMas.addEventListener("click", function () {
-                    botonMasPresionado = true;
-                    cantEntradasParaComprar = hacerEntradasParaComprar(cantEntradasParaComprar,botonMenosPresionado, botonMasPresionado);
-                    botonMasPresionado = false;
-
-                    actualizarCantidadEntradas();
-                });
-
-                
-            }
-
-            eventoSeleccionado = evento;
-            entradasDisponibles = eventoSeleccionado.entradasDisponibles;
-            ComprarCarrito();
-
-            const modal = new bootstrap.Modal(infoEvento);
-            modal.show();
-            document.querySelector(".modal-dialog").style.marginTop = "20px";
-        });
+        comprarBtnContainer.querySelector('button').addEventListener('click', () => {// Agregar evento de clic al botón
+            agregarAlCarrito(p.id);
+        })
     }
 
 
 
 
+}
+function agregarAlCarrito(id) {
 
-    if (evento && evento.titulo) {
-        title.textContent = eventoSeleccionado.titulo;
+
+    let producto = eventos.find(producto => producto.id == id);
+    let productoEnCarrito = CARRITO.find(producto => producto.id == id);
+
+    if (productoEnCarrito && productoEnCarrito.cantidad < 4) {
+        // Si el producto ya esta en el carrito
+        productoEnCarrito.cantidad++;
+
+    } else if (productoEnCarrito && productoEnCarrito.cantidad >= 4) {
+        // se establece el limite de que no se pueden comprar mas de cuatro
+        alert(" Ya compraste el maximo de entradas ")
     }
-    console.log("Las entradas restantes luego de su compra para "
-        + eventos[eventoSeleccionado.titulo] + " son " + entradasDisponibles);
-    eventos[eventoSeleccionado.titulo].entradasDisponibles = entradasDisponibles;
-});
+    else {
+        // La primera vez al agregar un producto
+        producto.cantidad = 1;
+        CARRITO.push(producto);
+    }
+
+    renderizarCarrito(productoEnCarrito);
+    guardarCarritoEnLocalStorage();
+}
+function renderizarCarrito(cantidad) {
+
+    const $carrito = d.getElementById("carrito");
+
+    $carrito.innerHTML = '';
+
+    CARRITO.forEach((p, index) => {
+
+        let producto = d.createElement('div')
+        producto.classList.add("card");
+
+        producto.innerHTML = `
+        <div  class="modal-dialog modal-dialog-centered"><div class="modal-content"><div class="modal-header text-center">
+       </div><div class="modal-body"><div class="bg-purple rounded p-3 text-center">
+                <h1 id= "centrar"  class="mb-0"> ${p.artista}</h1>
+                <h2 id= "centrar"  class="mb-0"> Entradas para comprar:  ${p.cantidad}</h2>
+                  <p class="mb-0"> ${p.titulo}</p>
+                  <p class="mb-0">Ubicación: ${p.ubicacion}</p>
+                  <p class="mb-0">Horario: ${p.horario}</p>
+                  <p class="mb-0">Entradas disponibles: ${p.entradasDisponibles}</p>
+                  <p class="mb-0">Precio: ${p.precio}</p>
+                  <p class="mb-0">Id: ${p.id}</p>
+                  <p class="mb-0">Día: ${p.dia}</p></div></div></div></div>
+            <button id="${p.id}" class="w" >Eliminar del carrito</button>
+        `
+        $carrito.appendChild(producto);
+
+        producto.querySelector('button').addEventListener('click', () => {
+            eliminarProductoDelCarrito(index);
+        })
+    })
+
+}
+function eliminarProductoDelCarrito(indice) {
+
+    CARRITO[indice].cantidad--;
+
+    if (CARRITO[indice].cantidad === 0) {
+        CARRITO.splice(indice, 1);
+    }
+    renderizarCarrito();
+    guardarCarritoEnLocalStorage();
+}
+function guardarCarritoEnLocalStorage() {
+    localStorage.setItem('carrito', JSON.stringify(CARRITO));
+}
+function cargarCarritoDeLocalStorage() {
+
+    if (localStorage.getItem('carrito') !== null) {
+        CARRITO = JSON.parse(localStorage.getItem('carrito'))
+    } else {
+        CARRITO = [];
+    }
+}
