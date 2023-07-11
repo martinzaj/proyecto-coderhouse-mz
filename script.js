4b3ec4fecbd02556d0f7eea4107f85affa81db33
martinzaj <mzajdenberg@frba.utn.edu.ar>
2023-07-11 14:42:51 -0300

    sweer alert agregado

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", cargaInicial);
 let Precioserivice = 77.7;
 function cargaInicial() {
@@ -7,7 +5,6 @@ function cargaInicial() {
     renderizarProductos();
     renderizarCarrito();
 }
-
 class Evento {
     constructor(id, titulo, ubicacion, horario, entradasDisponibles, precio, artista, dia) {
         this.id = id;
@@ -34,32 +31,29 @@ const eventos = [
     new Evento(11, "Fiesta Electrónica", "Club Nocturno XYZ", "22:00", 800, 500, "DJ ElectroMan", "2/1/24"),
     new Evento(12, "Gala de Ópera", "Gran Teatro", "19:30", 300, 2500, "Orquesta Sinfónica", "15/2/24"),
     new Evento(13, "Concierto de Pop", "Estadio Monumental", "19:00", 1500, 3500, "Beyonce", "10/3/24")
-    
+
 ]
 const d = document;
 let CARRITO = [];
-
-
- // Obtener referencias a los elementos del buscador
- const searchInput = document.getElementById("searchInput");
- const searchButton = document.getElementById("searchButton");
- searchButton.addEventListener('click', () => {
-   const searchText = searchInput.value; // Obtener el texto de búsqueda sin espacios en blanco
-   buscarEventosPorArtista(searchText);
- })
-
-function buscarEventosPorArtista(searchText){
+// Obtener referencias a los elementos del buscador
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener('click', () => {
+    const searchText = searchInput.value; // Obtener el texto de búsqueda sin espacios en blanco
+    buscarEventosPorArtista(searchText);
+})
+function buscarEventosPorArtista(searchText) {
     const artistasEncontrados = eventos.filter((el) => el.artista.includes(searchText)
     );
     const $tienda = d.getElementById("tienda");
     while ($tienda.firstChild) {
         $tienda.firstChild.remove();
-      }
+    }
 
-   
+
     let fila;
 
-artistasEncontrados.forEach((p, index) => {
+    artistasEncontrados.forEach((p, index) => {
 
         if (index % 3 === 0) {
             // Cada tres productos, creamos una nueva fila
@@ -110,16 +104,27 @@ artistasEncontrados.forEach((p, index) => {
         fila.appendChild(producto);
     })
 
-   
-}
 
+}
 //boton para comprar todos los productos del carrito
 const ConfBuy = document.getElementById("buy");
 ConfBuy.addEventListener('click', () => { // Agregar evento de clic al botón
-    let x = 0; x = calcularTotal()
-    if (confirm("quiere realizar la compra de sus productos? La cantidad de dindero a pagar es: " + x)) {
-        comprarLosProdutos()
-    }
+    let x = calcularTotal();
+    Swal.fire({
+        icon: 'question',
+        title: 'Confirmar compra',
+        text: `¿Deseas realizar la compra de tus productos? La cantidad de dinero a pagar es: ${x}`,
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Comprar',
+        cancelButtonText: 'Cancelar',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            comprarLosProdutos();
+        }
+    });
+
 });
 function calcularTotal() {
     let PrecioAPagar = 0;
@@ -134,6 +139,18 @@ function comprarLosProdutos() {
     CARRITO = []; // Vaciar el carrito después de calcular el precio total
     renderizarCarrito();
     guardarCarritoEnLocalStorage();
+    mostrarAvisoDeLaCompraConfirmada();
+}
+function mostrarAvisoDeLaCompraConfirmada(){
+    Swal.fire({
+        icon: 'custom',
+        title: '<i class="fas fa-heart"></i> Se confirmó tu compra',
+        showConfirmButton: false,
+        timer: 7000,
+        customClass: {
+          title: 'swal2-heart-title'
+        }
+      });
 }
 function renderizarProductos() {
 
@@ -192,12 +209,12 @@ function renderizarProductos() {
     })
 }
 function generarEvento(p) {
-        //generacion de la informacion del evento particular que se selecciono
-        const infoEvento = document.getElementById("info-evento");
+    //generacion de la informacion del evento particular que se selecciono
+    const infoEvento = document.getElementById("info-evento");
 
-        infoEvento.classList.add("mb-3");
-        infoEvento.classList.add("custom-modal-content");
-        infoEvento.innerHTML = `<div calass="modal-dialog modal-dialog-centered"><div class="modal-content"><div class="modal-header text-center">
+    infoEvento.classList.add("mb-3");
+    infoEvento.classList.add("custom-modal-content");
+    infoEvento.innerHTML = `<div calass="modal-dialog modal-dialog-centered"><div class="modal-content"><div class="modal-header text-center">
   <h5 class="modal-title text-center">Información del evento</h5></div><div class="modal-body"><div class="bg-purple rounded p-3 text-center">
             <p class="mb-0">Título: ${p.titulo}</p>
             <p class="mb-0">Ubicación: ${p.ubicacion}</p>
@@ -207,15 +224,14 @@ function generarEvento(p) {
             <p class="mb-0">Artista: ${p.artista}</p>
             <p class="mb-0">Id: ${p.id}</p>
             <p class="mb-0">Día: ${p.dia}</p></div></div></div></div>`;
-        //boton de agregar al carrito
-        const comprarBtnContainer = document.getElementById("comprar-btn-container");
-        comprarBtnContainer.classList.add("mt-3");
-        comprarBtnContainer.innerHTML = ` <div class="text-center"   id="comprar-btn">
+    //boton de agregar al carrito
+    const comprarBtnContainer = document.getElementById("comprar-btn-container");
+    comprarBtnContainer.classList.add("mt-3");
+    comprarBtnContainer.innerHTML = ` <div class="text-center"   id="comprar-btn">
         <button id="${p.id}" class="btn btn-primary mt-3">Agregar al carrito</button> </div>`;
 
-    comprarBtnContainer.querySelector('button').addEventListener('click', () => {agregarAlCarrito(p.id); }) 
+    comprarBtnContainer.querySelector('button').addEventListener('click', () => { agregarAlCarrito(p.id); })
 }
-
 function renderizarCarrito(cantidad) {
     const $carrito = d.getElementById("carrito");
     $carrito.innerHTML = '';
@@ -254,7 +270,19 @@ function eliminarProductoDelCarrito(indice) {
         CARRITO.splice(indice, 1);
     }
     renderizarCarrito();
+    MostrarProductoQuitado()
     guardarCarritoEnLocalStorage();
+    
+}
+function MostrarProductoQuitado(){
+    Swal.fire({
+        position: 'bottom-end',
+        icon: 'success',
+        title: 'Se ha quitado un elemento del carrito',
+        showConfirmButton: false,
+        timer: 2000,
+        toast: true
+      });
 }
 function guardarCarritoEnLocalStorage() {
     localStorage.setItem('carrito', JSON.stringify(CARRITO));
@@ -267,21 +295,19 @@ function cargarCarritoDeLocalStorage() {
         CARRITO = [];
     }
 }
-
-import Swal from 'sweetalert';
 function agregarAlCarrito(id) {
     let producto = eventos.find(producto => producto.id == id);
     let productoEnCarrito = CARRITO.find(producto => producto.id == id);
 
     if (productoEnCarrito && productoEnCarrito.cantidad < 4) {
         // Si el producto ya esta en el carrito
-
-        productoEnCarrito.cantidad++;
         mostrarAgregadoACarrito()
+        productoEnCarrito.cantidad++;
+
 
     } else if (productoEnCarrito && productoEnCarrito.cantidad >= 4) {
         // se establece el limite de que no se pueden comprar mas de cuatro
-        alert(" Ya compraste el maximo de entradas ")
+        mostrarMaximoProducto()
     }
     else {
         // La primera vez al agregar un producto
@@ -293,15 +319,29 @@ function agregarAlCarrito(id) {
     renderizarCarrito(productoEnCarrito);
     guardarCarritoEnLocalStorage();
 }
-
 function mostrarAgregadoACarrito() {
-  Swal.fire({
-    toast: true,
-    position: 'top-end',
-    icon: 'success',
-    title: '¡Producto agregado al carrito!',
-    showConfirmButton: false,
-    timer: 2000
-  });
-}
+    Swal.fire({
+        toast: true,
+        position: 'top-end',
+        icon: 'success',
+        title: '¡Producto agregado al carrito!',
+        showConfirmButton: false,
+        timer: 2400
+    });
 
+}
+function mostrarMaximoProducto() {
+    Swal.fire({
+        position: 'top-start', // Cambiar la posición a 'top-start' para que aparezca en la parte superior izquierda
+        icon: 'error',
+        title: 'Ya has alcanzado el límite de máximo 4 de entradas',
+        showConfirmButton: false,
+        timer: 5000,
+        toast: true,
+        customClass: {
+            icon: 'swal2-icon swal2-animate-error-icon',
+            title: 'swal2-x-error-title'
+        }
+    });
+
+}
